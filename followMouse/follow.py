7c71c71df8d7f8166fcdeb0489ba9dcb8cc95406
Searoges <connorseanrodgers@gmail.com>
2017-10-17 09:47:47 +0100

    Code Redundancy Reduced

--- followMouse/follow.py
+++ followMouse/follow.py
@@ -5,7 +5,7 @@ import math
 from pygame.locals import *
 
 
-class Cat(object):
+class Player(object):
     def __init__(self):
         self.image = pygame.image.load('ball.png')
         self.x = 1
@@ -14,37 +14,29 @@ class Cat(object):
         self.rect = self.image.get_rect()
 
     def draw(self, surface):
-        mosx = 0
-        mosy = 0
-        x,y = pygame.mouse.get_pos()
-        mosx = (x - self.x)
-        mosy = (y - self.y)
-        #self.x = 0.9*self.x + mosx
-        #self.y = 0.9*self.y + mosy
         rotImage = pygame.transform.rotate(self.image, (50*self.angle))
-        rot_rect = rotImage.get_rect(center=self.rect.center)
-        self.x = (rot_rect.x)
-        self.y = (rot_rect.y)
+        rotRect = rotImage.get_rect(center = self.rect.center)
+        self.x = (rotRect.x)
+        self.y = (rotRect.y)
         surface.blit(rotImage, (self.x, self.y))
         pygame.display.update()
-        #print self.angle
 
 
 pygame.init()
 screen = pygame.display.set_mode((800,600))
-cat = Cat()
-Clock = pygame.time.Clock()
+objPlayer = Player()
+clock = pygame.time.Clock()
 
 running = True
 while running:
     screen.fill((255,255,255))
-    cat.draw(screen)
+    objPlayer.draw(screen)
 
     for event in pygame.event.get():
         if event.type == MOUSEMOTION:
             mouseX, mouseY = pygame.mouse.get_pos()
 
-            cat.angle = math.atan2((cat.x - mouseX), (cat.y - mouseY))
+            objPlayer.angle = math.atan2((objPlayer.x - mouseX), (objPlayer.y - mouseY))
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -52,4 +44,4 @@ while running:
             sys.exit()
 
     pygame.display.update()
-    Clock.tick(40)
+    clock.tick(40)
